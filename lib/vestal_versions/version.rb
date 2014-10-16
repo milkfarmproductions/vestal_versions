233fb10d70413dee4ee2ec657c39e2f80c519d68
Neil Gupta <neil@tabuleapp.com>
2014-10-15 21:25:30 -0500

    Use set_table_name

--- lib/vestal_versions/version.rb
+++ lib/vestal_versions/version.rb
@@ -7,7 +7,7 @@ module VestalVersions
     include Comparable
     include ActiveSupport::Configurable
 
-    self.table_name = "vestal_versions"
+    set_table_name "vestal_versions"
 
     # Associate polymorphically with the parent record.
     belongs_to :versioned, :polymorphic => true
